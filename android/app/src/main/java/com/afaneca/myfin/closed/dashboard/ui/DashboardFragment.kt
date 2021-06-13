4a744246126facc9dbabf6f9ea2cb4ab13f96d31
António Faneca <me@afaneca.com>
2021-06-13 13:00:38 +0100

    * fixed issue with dashboard being recreated on orientation change

--- android/app/src/main/java/com/afaneca/myfin/closed/dashboard/ui/DashboardFragment.kt
+++ android/app/src/main/java/com/afaneca/myfin/closed/dashboard/ui/DashboardFragment.kt
@@ -5,18 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.lifecycle.LiveData
 import com.afaneca.myfin.R
 import com.afaneca.myfin.base.BaseFragment
 import com.afaneca.myfin.closed.dashboard.data.DashboardRepository
 import com.afaneca.myfin.data.network.MyFinAPIServices
 import com.afaneca.myfin.data.network.Resource
 import com.afaneca.myfin.databinding.FragmentDashboardBinding
-import com.afaneca.myfin.open.login.data.LoginRepository
 import com.afaneca.myfin.utils.visible
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.runBlocking
-import java.time.YearMonth
 import java.util.*
 
 class DashboardFragment :
@@ -42,20 +37,15 @@ class DashboardFragment :
     private fun getMonthlyIncomeExpensesDistributionDataForCurrentMonth() {
         val currentMonth = Calendar.getInstance().get(Calendar.MONTH);
         val currentYear = Calendar.getInstance().get(Calendar.YEAR);
-        Toast.makeText(
-            requireContext(),
-            "Month: $currentMonth and Year: $currentYear",
-            Toast.LENGTH_LONG
-        ).show()
-        viewModel.getMonthlyExpensesIncomeDistribution(currentMonth, currentYear)
+        viewModel.requestMonthlyExpensesIncomeDistribution(currentMonth, currentYear)
     }
 
     private fun bindListeners() {
-        viewModel.monthlyIncomeExpensesDistributionData.observe(viewLifecycleOwner, {
+        viewModel.getMonthlyIncomeExpensesDistributionData().observe(viewLifecycleOwner, {
             binding.loadingPb.visible(it is Resource.Loading)
             when (it) {
                 is Resource.Success -> {
-                    Toast.makeText(requireContext(), "${it.data}", Toast.LENGTH_SHORT).show()
+
                 }
                 is Resource.Failure -> {
                     Toast.makeText(requireContext(), it.errorMessage, Toast.LENGTH_LONG).show()
--- android/app/src/main/java/com/afaneca/myfin/closed/dashboard/ui/DashboardViewModel.kt
+++ android/app/src/main/java/com/afaneca/myfin/closed/dashboard/ui/DashboardViewModel.kt
@@ -8,20 +8,24 @@ import com.afaneca.myfin.closed.dashboard.data.DashboardRepository
 import com.afaneca.myfin.closed.dashboard.data.MonthlyIncomeExpensesDistributionResponse
 import com.afaneca.myfin.data.network.Resource
 import com.afaneca.myfin.open.login.data.AttemptLoginResponse
+import com.afaneca.myfin.utils.SingleLiveEvent
 import kotlinx.coroutines.launch
 
 class DashboardViewModel(
     private val repository: DashboardRepository
 ) : ViewModel() {
 
-    private val _monthlyIncomeExpensesDistributionData: MutableLiveData<Resource<MonthlyIncomeExpensesDistributionResponse>> =
-        MutableLiveData()
-    val monthlyIncomeExpensesDistributionData: LiveData<Resource<MonthlyIncomeExpensesDistributionResponse>>
-        get() = _monthlyIncomeExpensesDistributionData
+    private val _monthlyIncomeExpensesDistributionData: SingleLiveEvent<Resource<MonthlyIncomeExpensesDistributionResponse>> =
+        SingleLiveEvent()
 
-    fun getMonthlyExpensesIncomeDistribution(month: Int, year: Int) = viewModelScope.launch {
-        _monthlyIncomeExpensesDistributionData.value = repository.getMonthlyExpensesIncomeDistribution(
-            month, year
-        )
+    fun getMonthlyIncomeExpensesDistributionData(): SingleLiveEvent<Resource<MonthlyIncomeExpensesDistributionResponse>> =
+        _monthlyIncomeExpensesDistributionData
+
+
+    fun requestMonthlyExpensesIncomeDistribution(month: Int, year: Int) = viewModelScope.launch {
+        _monthlyIncomeExpensesDistributionData.value =
+            repository.getMonthlyExpensesIncomeDistribution(
+                month, year
+            )
     }
 }
