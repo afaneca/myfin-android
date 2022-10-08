ad8da94e5ef883821bfef34b1dc7d4550f1c2f35
António Faneca <me@afaneca.com>
2022-10-08 16:52:14 +0100

    updates proguard rules to fix crashes in prodRelease build

--- android/app/src/main/java/com/afaneca/myfin/closed/accounts/data/AccountsListResponse.kt
+++ android/app/src/main/java/com/afaneca/myfin/closed/accounts/data/AccountsListResponse.kt
@@ -1,6 +1,7 @@
 package com.afaneca.myfin.closed.accounts.data
 
+import androidx.annotation.Keep
 import com.afaneca.myfin.base.objects.MyFinAccount
 
-
+@Keep
 class AccountsListResponse : ArrayList<MyFinAccount>()
